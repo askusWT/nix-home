f37323e54bf8972eca78da2d48f914baded0f3be
askusWT <63749667+askusWT@users.noreply.github.com>
2024-05-03 12:29:30 +0100

    Update flake.nix
    initial set up

--- flake.nix
+++ flake.nix
@@ -1,24 +1,8 @@
 {
-  description = "TOI Home Manager configuration";
-
-  nixConfig = {
-    experimental-features = [ "nix-command" "flakes" ];
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://nix-gaming.cachix.org"
-      "https://hyprland.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-    ];
-  };
+  description = "Home Manager Configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    comma.url = "github:nix-community/comma";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -41,10 +25,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-colors.url = "github:misterio77/nix-colors";
-    nix-gaming = {
-      url = "github:fufexan/nix-gaming";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nix-index-database = {
       url = "github:Mic92/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -62,233 +42,34 @@
     vscode-server.url = "github:nix-community/nixos-vscode-server";
   };
 
-  outputs = { self, nixpkgs, flake-utils, home-manager, ... }@inputs:
-    let
-      inherit (self) outputs;
-      gn = "dave";
-      gnsn = "daveconroy";
-      handle = "tiredofit";
-
-      pkgsForSystem = system: import nixpkgs {
-        overlays = [
-          inputs.comma.overlays.default
-          inputs.nur.overlay
-          inputs.nix-vscode-extensions.overlays.default
-          inputs.nixpkgs-wayland.overlay
-        ];
-        inherit system;
-      };
-
-      HomeConfiguration = args: home-manager.lib.homeManagerConfiguration (rec {
-        modules = [
-          (import ./home)
-          (import ./modules)
-        ];
-        extraSpecialArgs = { };
-        pkgs = pkgsForSystem (args.system or "x86_64-linux");
-      } // { inherit (args) extraSpecialArgs; });
-    in
-      flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ] (system: rec {
-        legacyPackages = pkgsForSystem system;
-      }) //
-      {
+  outputs = { self, nixpkgs, home-manager, flake-utils, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {};
+        }; 
+        username = "m4rknewt0n";
+        hmConfig = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          system = system;
+          homeDirectory = "/home/${username}"; 
+          stateVersion = "24.05";               
+          configuration = {
+            imports = [
+              ./dotfiles  // Assuming this is structured to be imported
+              ./home
+              ./modules
+            ];
+            programs.git.enable = true;
+            # Add other programs and configurations
+          };
+        };
+      in
+        {
         homeConfigurations = {
-          "beef.${gn}" = HomeConfiguration {
-            extraSpecialArgs = {
-              org = "toi";
-              role = "workstation";
-              hostname = "beef";
-              username = gn;
-              displays = 3;
-              display_center = "DisplayPort-1";
-              display_left = "DisplayPort-2";
-              display_right = "HDMI-A-0";
-              networkInterface = "wlp10s0";
-              inherit inputs outputs;
-            };
-          };
-
-          "butcher.${gn}" = HomeConfiguration {
-            extraSpecialArgs = {
-              org = "toi";
-              role = "server";
-              hostname = "butcher";
-              username = gn;
-              networkInterface = "enp6s18";
-              inherit inputs outputs;
-            };
-          };
-
-          "cog.${gn}" = HomeConfiguration {
-            extraSpecialArgs = {
-              org = "toi";
-              role = "server";
-              hostname = "cog" ;
-              username = gn;
-              networkInterface = "br0";
-              inherit inputs outputs;
-            };
-          };
-
-          "nakulaptop" = {
-            "${gn}" = HomeConfiguration {
-              extraSpecialArgs = {
-                org = "toi";
-                role = "workstation";
-                hostname = "nakulaptop";
-                username = gn;
-                displays = 2;
-                display_center = "HDMI-A-0";
-                display_right = "eDP";
-                networkInterface = "wlo1";
-                inherit inputs outputs;
-              };
-            };
-            "ireen" = HomeConfiguration {
-              extraSpecialArgs = {
-                org = "toi";
-                role = "workstation";
-                hostname = "nakulaptop";
-                username = "ireen";
-                displays = 1;
-                display_center = "eDP";
-                display_right = "HDMI-A-0";
-                networkInterface = "wlo1";
-                inherit inputs outputs;
-              };
-            };
-          };
-
-          "seed.${gn}" = HomeConfiguration {
-            extraSpecialArgs = {
-              org = "toi";
-              role = "server";
-              hostname = "seed" ;
-              username = gn;
-              networkInterface = "enp1s0f0";
-              inherit inputs outputs;
-            };
-          };
-
-          "tentacle.${gn}" = HomeConfiguration {
-            system = "aarch64-linux";
-            extraSpecialArgs = {
-              org = "toi";
-              role = "server";
-              hostname = "tentacle" ;
-              username = gn;
-              networkInterface = "enp6s18";
-              inherit inputs outputs;
-            };
-          };
-
-          "workspace.${gn}" = HomeConfiguration {
-            extraSpecialArgs = {
-              org = "toi";
-              role = "server";
-              hostname = "workspace" ;
-              username = gn;
-              networkInterface = "br0";
-              inherit inputs outputs;
-            };
-          };
-
-      ##
-          "bell.${gnsn}" = HomeConfiguration {
-            extraSpecialArgs = {
-              org = "sd";
-              role = "server";
-              hostname = "bell";
-              username = gnsn;
-              inherit inputs outputs;
-            };
-          };
-
-          "edge.${gnsn}" = HomeConfiguration {
-            extraSpecialArgs = {
-              org = "sd";
-              role = "server";
-              hostname = "edge";
-              username = gnsn;
-              inherit inputs outputs;
-            };
-          };
-
-          "einstein.${gnsn}" = HomeConfiguration {
-            system = "aarch64-linux";
-            extraSpecialArgs = {
-              org = "sd";
-              role = "server";
-              hostname = "einstein";
-              username = gnsn;
-              inherit inputs outputs;
-            };
-          };
-
-          "sd20.${gnsn}" = HomeConfiguration {
-            extraSpecialArgs = {
-              org = "sd";
-              role = "server";
-              hostname = "sd20";
-              username = gnsn;
-              inherit inputs outputs;
-            };
-          };
-
-          "sd91.${gnsn}" = HomeConfiguration {
-            extraSpecialArgs = {
-              org = "sd";
-              role = "server";
-              hostname = "sd91";
-              username = gnsn;
-              inherit inputs outputs;
-            };
-          };
-
-          "sd102.${gnsn}" = HomeConfiguration {
-            extraSpecialArgs = {
-              org = "sd";
-              role = "server";
-              hostname = "sd102";
-              username = gnsn;
-              inherit inputs outputs;
-            };
-          };
-
-          "sd111.${gnsn}" = HomeConfiguration {
-            extraSpecialArgs = {
-              org = "sd";
-              role = "server";
-              hostname = "sd111";
-              username = gnsn;
-              inherit inputs outputs;
-            };
-          };
-
-          "tesla.${gnsn}" = HomeConfiguration {
-            extraSpecialArgs = {
-              org = "sd";
-              role = "server";
-              hostname = "tesla";
-              username = gnsn;
-              inherit inputs outputs;
-            };
-          };
-
-          ###
-
-          "lambda-quad.${handle}" = HomeConfiguration {
-            extraSpecialArgs = {
-              org = "sr";
-              role = "server";
-              hostname = "lamda-quad";
-              username = handle;
-              inherit inputs outputs;
-            };
-          };
-      };
-
-      inherit home-manager;
-      inherit (home-manager) packages;
-    };
+           "${username}" = hmConfig;
+        };
+      }
+    );
 }
